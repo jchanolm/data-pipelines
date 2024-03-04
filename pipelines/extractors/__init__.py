8b22e901894d962cf96f9ca04c721d15a7cd4abe
Jordan <editor@apertif.co>
2024-03-03 20:10:32 -0800

    changes

--- pipelines/extractors/__init__.py
+++
@@ -1,8 +0,0 @@
-import logging 
-import os 
-from dotenv import load_dotenv
-
-load_dotenv()
-logging.basicConfig(level="INFO")
-if "LOGLEVEL" in os.environ:
-    logging.basicConfig(level=os.environ["LOGLEVEL"])
--- pipelines/helpers/__init__.py
+++
@@ -1,6 +0,0 @@
-from .s3 import S3Utils 
-from .utils import Utils 
-from .requests import Requests
-from .cypher import Cypher 
-from .decorators import *
-
