722ae8e5c52abc1b486fd2660993c6bfbed1574d
jchanolm <160365885+jchanolm@users.noreply.github.com>
2024-03-05 22:14:57 -0800

    Update scrape.py
    git struggles

--- pipelines/scraping/discourse/scrape.py
+++ pipelines/scraping/discourse/scrape.py
@@ -1,129 +1,150 @@
-from ..helpers import Scraper
-import logging 
+import requests
+import json
+import logging
 import time
-import os 
-import requests as requests
+from ..helpers import Scraper
+from .helpers.queries import proposals_query
 
-
-
-class DiscourseScraper(Scraper):
+class SnapshotScraper(Scraper):
     def __init__(self):
-        super().__init__(module_name='discourse')  # This calls the __init__ method of the Scraper class
-        self.base_url = "https://forum.arbitrum.foundation"
+        super().__init__(module_name='snapshot')
+        self.snapshot_api_url = "https://hub.snapshot.org/graphql"
+        self.arb_space_id = "arbitrumfoundation.eth"
+        logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        self.proposals_query = f"""
+        {{
+            proposals(
+                where: {{
+                    space_in: ["{self.arb_space_id}"]
+                }},
+                first: 1000
+            ) {{
+                id
+                title
+                body
+                choices
+                start
+                end
+                state
+                author
+            }}
+        }}
+        """ 
 
 
-    def fetch_categories(self):
-        base_url = self.base_url
-        url = f"{self.base_url}/categories.json"
-        response = requests.get(url)
-        if response.status_code != 200:
-            print(f"Failed to fetch categories from {base_url} with Status Code: {response.status_code}")
-            logging.error(f"Failed to fetch categories from {base_url}")
-        else:
-            category_data = response.json()
-            categories = category_data.get('category_list', {}).get('categories', [])
-            return categories 
+
+    def send_api_request(self, query, retry_count=0):
+        max_retries = 10
+        while retry_count <= max_retries:
+            try:
+                response = requests.post(self.snapshot_api_url, json={"query": query})
+                response.raise_for_status()  # Raises exception for 4XX or 5XX errors
+                data = response.json()
+                if "errors" in data:
+                    raise Exception(f"API response error: {data['errors']}")
+                return data["data"]
+            except requests.RequestException as e:
+                logging.error(f"Request error: {e}")
+                retry_count += 1
+                if retry_count > max_retries:
+                    logging.error("Max retries exceeded. Giving up.")
+                    return None
+            except Exception as e:
+                logging.error(f"Unhandled exception: {e}")
+                return None
+
+        def fetch_proposals(self):
+            logging.info("Fetching proposals...")
+            return self.send_api_request(self.proposals_query)
+
+        def get_all_votes_for_proposals(self, proposals):
+            votes_query_template = """
+            {{
+                votes (
+                    first: {0},
+                    skip: {1},
+                    orderBy: "created",
+                    orderDirection: desc,
+                    where: {{
+                        proposal_in: ["{2}"]
+                    }}
+                ) {{
+                    id
+                    ipfs
+                    voter
+                    created
+                    choice
+                    proposal {{
+                        id
+                        choices
+                    }}
+                    space {{
+                        id
+                        name
+                    }}
+                }}
+            }}
+            """
+            all_votes = []
+            for proposal in proposals:
+                proposal_id = proposal["id"]
+                proposal_title = proposal['title']
+                offset = 0
+                results = True
+                while results:
+                    query = votes_query_template.format(1000, offset, proposal_id)
+                    response = self.send_api_request(query)
+                    time.sleep(.5)
+                    if response:
+                        data = response["votes"]
+                        votes_count = len(data)
+                        if votes_count == 0:
+                            logging.info(f"No more votes found for {proposal_title}. Total votes retrieved: {len(all_votes)}")
+                            break
+                        try:
+                            if offset < 5000:
+                                all_votes.extend(data)
+                                logging.info(f"Retrieved {votes_count} votes for {proposal_title}. Total votes retrieved so far: {len(all_votes)}")
+                                offset += 1000
+                            else:
+                                logging.info(f"Reached maximum offset limit for {proposal_title}. Total votes retrieved: {len(all_votes)}")
+                                break
+                        except Exception as e:
+                            logging.error(f"Error fetching votes for {proposal_title} with offset {offset}: {str(e)}")
+                            break
+                    else:
+                        logging.error(f"Failed to fetch additional votes for {proposal_title}.")
+                        break
+            return all_votes
         
-
-    def fetch_topics(self, category_slug):
-        topics = []
-        page = 0 
-        while True:
-            url = f"{self.base_url}/c/{category_slug}.json?page={page}"
-            response = requests.get(url)
-            if response.status_code != 200: 
-                break 
-            topics_data = response.json()
-            fetched_topics = topics_data.get("topic_list", {}).get("topics", [])
-            if not fetched_topics:
-                break 
-            topics.extend(fetched_topics)
-            page += 1
-        return topics 
+    def refine_votes(self, votes):
+        refined_votes = []
+        for vote in votes:
+            refined_vote = {
+                'proposalId': vote['proposal']['id'],
+                'voter': vote['voter'].lower(),
+                'choice': vote['choice'],
+                'id': vote['id']
+            }
+            refined_votes.append(refined_vote)
+        return refined_votes
     
-    def fetch_topic_posts(self, topic_id):
-        url = f"{self.base_url}/t/{topic_id}.json"
-        response = requests.get(url)
-        time.sleep(.1) ## semi-respectful pause between requests
-        if response.status_code != 200:
-            return None 
-        return response.json()
-
-    def organize_post_data(self, topic_posts, category_name):
-            posts_data = []
-            posts = topic_posts.get("post_stream", {}).get("posts", [])
-            for post in posts:
-                post_data = {
-                    "text": post.get("cooked", ""),
-                    "title": topic_posts.get("title", ""),
-                    "author": post.get("username", ""),
-                    "tags": topic_posts.get("tags", []),
-                    "category": category_name,  # Include category name
-                    "topic_id": topic_posts.get("id"),  # Include topic ID
-                    "url": f"{self.base_url}/t/{topic_posts.get('id')}/{post.get('post_number')}"
-                }
-                # Find responses to this post
-                responses = [response for response in posts if response.get("reply_to_post_number") == post.get("post_number")]
-                post_data["responses"] = [{
-                    "text": response.get("cooked", ""),
-                    "author": response.get("username", "")
-                } for response in responses]
-                posts_data.append(post_data)
-            return posts_data
-
-    def fetch_posts(self):
-        print("Fetching categories....")
-        categories = self.fetch_categories()
-        all_posts_data = []
-        for category in categories:
-            category_slug = category.get('slug')
-            category_name = category.get('name')
-            print(f"Fetching posts for {category_name}.....")
-            topics = self.fetch_topics(category_slug)
-            for topic in topics:
-                topic_id = topic.get('id')
-                topic_details = self.fetch_topic_posts(topic_id)
-                if topic_details:
-                    posts_data = self.organize_post_data(topic_details, category_name)
-                    all_posts_data.extend(posts_data)
-                    print(f"Scraped {len(all_posts_data)} records....")
-        return all_posts_data
-    
-    def extract_posts(self, posts_data):
-            posts_without_responses = []
-            for post in posts_data:
-                post_copy = post.copy()  # Make a copy to avoid modifying the original data
-                post_copy.pop('responses', None)  # Remove responses
-                posts_without_responses.append(post_copy)
-                return posts_without_responses
-
-    def extract_post_responses(self, posts_data):
-        responses = []
-        for post in posts_data:
-            for response in post.get('responses', []):
-                response_copy = response.copy()  # Make a copy to avoid modifying the original data
-                # Add a reference to the parent post
-                response_copy['parent_post_title'] = post['title']
-                response_copy['parent_post_id'] = post['topic_id']
-                responses.append(response_copy)
-        return responses
-    
-    def get_last_post_number(self, posts_data):
-        last_post_number = max([post.get('post_number', 0) for post in posts_data])
-        return last_post_number
-
 
     def run(self):
-        print(f"Fetching all posts from {self.base_url}...")
-        posts = self.fetch_posts()
-        self.data['posts'] = self.extract_posts(posts)
-        self.data['responses'] = self.extract_post_responses(posts)
-        self.metadata['last_post_number'] = self.get_last_post_number(posts)
-
+        proposals_response = self.fetch_proposals()
+        proposals = proposals_response.get('proposals', [])
+        all_votes = self.get_all_votes_for_proposals(proposals)
+        refined_votes = self.refine_votes(all_votes)
+        self.data['proposals'] = proposals 
+        self.data['votes'] = refined_votes
+        if proposals:
+            most_recent_proposal = max(proposals, key=lambda x: x['end'])
+            self.metadata['last_proposal_end_dt'] = most_recent_proposal['end']
+            self.metadata['last_proposal_id'] = most_recent_proposal['id']
+            logging.info(f"Total votes fetched and refined: {len(refined_votes)}")
         self.save_data_local()
         self.save_metadata_local()
 
-
 if __name__ == "__main__":
-    S = DiscourseScraper()
-    S.run()
+    scraper = SnapshotScraper()
+    scraper.run()
