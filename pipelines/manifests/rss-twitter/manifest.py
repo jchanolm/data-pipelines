42e0ebef9d81f9145937294cd527727144df8bf0
= <jochanolm@gmail.com>
2024-04-08 20:58:55 -0700

    added fetch feeds to manifest.py

--- pipelines/manifests/rss-twitter/manifest.py
+++ pipelines/manifests/rss-twitter/manifest.py
@@ -13,7 +13,29 @@ class EntityManifest(Manifest):
         self.cyphers = RssTwitterCyphers()
         super().__init__("manifest-rss-twitter")
 
-    def get_feeds_with_offset(self):
+
+    def create_feed_for_twitter_handle(self, url):
+        payload={
+            "url": url
+        }
+        headers = {
+        'Authorization': 'Bearer c_rtuymP25PLbcg9:s_CrIPmhZvTPHi8Ge1JPcIWr',
+        'Content-Type': 'application/json'
+        }
+        try: 
+            response = r.post(url, headers=headers, json=payload)
+            if response.status_code != 200:
+                with Exception as e:
+                    logging.error(f"Feed creation API call returned error: {e}...")
+            else:
+                return response
+        except Exception as e:
+            logging.error(f"Exception creating feed: {e}")
+        return response
+
+
+
+    def get_feeds_with_offset():
         limit = 100
         offset = 0
         feeds_list = []
@@ -24,6 +46,7 @@ class EntityManifest(Manifest):
                 'Authorization': 'Bearer c_rtuymP25PLbcg9:s_CrIPmhZvTPHi8Ge1JPcIWr'
             }
             response = r.request("GET", url, headers=headers, data=payload)
+            time.sleep(2.5)
             feeds = response.json()
             print(feeds)
             if feeds['total'] == 0:
@@ -31,6 +54,7 @@ class EntityManifest(Manifest):
             feeds_list.extend(feeds['data'])
             offset += limit
         return feeds_list
+    
     def create_feed_for_twitter_handle(self, url):
             payload={
                 "url": url
